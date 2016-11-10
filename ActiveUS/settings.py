3e0527732e72c03b66117057f6c92c0327f5bda1
sseaver <sseaver321@gmail.com>
2016-11-10 14:28:20 -0500

    started working on models

--- ActiveUS/settings.py
+++ ActiveUS/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'multiselectfield',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'ActiveUS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- app/models.py
+++ app/models.py
@@ -1,3 +1,36 @@
 from django.db import models
-
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+
+
+class Sport(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Profile(models.Model):
+    user = models.ForeignKey('auth.User')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    age = models.IntegerField()
+    fav_sports = models.ManyToManyField(Sport)
+
+
+@receiver(post_save, sender="auth.User")
+def create_user_profile(**kwargs):
+    created = kwargs.get('created')
+    instance = kwargs.get('instance')
+    if created:
+        Profile.objects.create(user=instance)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=100)
+    map_url = models.URLField()
+
+
+class Event(models.Model):
+    date = models.DateField(auto_now=False)
+    time = models.TimeField(auto_now=False)
+    location = models.CharField(max_length=100)
+    participants = models.ManyToManyField('auth.User')
